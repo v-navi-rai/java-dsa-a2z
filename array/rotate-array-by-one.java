f38b7061b4756bc8972f396fadcc89523886eb6a
v-navi-rai <raivaishnavi2004@gmail.com>
2025-12-16 20:51:50 +0530

    solved rotate-array-by-one problem

---
+++ array/rotate-array-by-one.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void rotate(int[] arr) {
+        // code here
+        int n=arr.length;
+        int temp=arr[n-1];
+        for(int i=n-1; i>=1; i--){
+            arr[i]=arr[i-1];
+        }
+        arr[0]=temp;
+    }
+}
